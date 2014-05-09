f9c85cb1d99430849417da8d5c48e8043a6cf441
kimkennedy06 <kimberly.r.kennedy@gmail.com>
2014-05-08 21:59:13 -0700

    Extract only data with certain dates
    Extract only data from household_power_consumption.txt that has the
    dates of Feb 1, 2007 or Feb 2, 2007

---
+++ plot1.R
@@ -0,0 +1,15 @@
+#Check to see if package "sqldf" is installed
+#If not installed, install "sqldf"
+if("sqldf" %in% rownames(installed.packages())==FALSE)
+{
+  install.packages.("sqldf")
+}
+#Run library sqldf and datasets
+library(sqldf)
+library(datasets)
+
+#Create SQL call to pull only lines with Dates of Feb 1, 2007 or Feb 2,2007
+specificDates<-"Select * from file where Date='1/2/2007' or Date='2/2/2007'"
+#Create data frame with only the lines with specific dates found from SQL
+PowerConsumption<-read.csv2.sql("household_power_consumption.txt",sql=specificDates,header=TRUE,sep=";")
+
