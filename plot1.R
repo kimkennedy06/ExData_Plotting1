50cd6920baa4d19fb609c24b906bee316b779cf6
kimkennedy06 <kimberly.r.kennedy@gmail.com>
2014-05-08 22:57:45 -0700

    Adding DateTime column to data frame
    Added DateTime column to data frame, converted DateTime class to Date
    class, removed Date and Time columns, reordered data frame

--- plot1.R
+++ plot1.R
@@ -12,6 +12,17 @@ library(datasets)
 specificDates<-"Select * from file where Date='1/2/2007' or Date='2/2/2007'"
 #Create data frame with only the lines with specific dates found from SQL
 PowerConsumption<-read.csv2.sql("household_power_consumption.txt",sql=specificDates,header=TRUE,sep=";")
+#Create column that combines Date and Time columns
+PowerConsumption$DateTime<-sapply(PowerConsumption,function(x)paste(PowerConsumption$Date,PowerConsumption$Time,sep=" "))[,1]
+
+#Change Class of DateTime to class Date
+PowerConsumption$DateTime<-as.Date(PowerConsumption$DateTime,'%d/%m/%Y %H:%M:%S')
+
+#Remove Date and Time columns since created merged column of date and time
+PowerConsumption<-PowerConsumption[,-c(1,2)]
+
+#Reorder so that DateTime is first
+PowerConsumption<-PowerConsumption[,c(length(PowerConsumption),3:length(PowerConsumption)-1)]
 
 #Open png device and create 'plot1.png' in present working directory
 png(file="plot1.png",width=480,height=480,units="px")
