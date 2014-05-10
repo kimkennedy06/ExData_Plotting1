e9b4fd5f79555f3ee14c20a1d46323e3ca796085
kimkennedy06 <kimberly.r.kennedy@gmail.com>
2014-05-10 10:13:28 -0700

    Added spacing for readability

--- plot4.R
+++ plot4.R
@@ -27,21 +27,27 @@ PowerConsumption<-PowerConsumption[,-c(1,2)]
 PowerConsumption<-PowerConsumption[,c(length(PowerConsumption),1:length(PowerConsumption)-1)]
 
 #Open png device and create 'plot2.png' in present working directory
-png(file="plot3.png",width=480,height=480,units="px")
+png(file="plot4.png",width=480,height=480,units="px")
 par(mfrow=c(2,2))
 with(PowerConsumption,{
   #Create Line Plot of Global Active Power over DateTime with y-axis label
-  plot(DateTime,Global_active_power,xlab="",ylab="Global Active Power (kilowatts)",type="l")
+  plot(DateTime,Global_active_power,xlab="",ylab="Global Active Power",type="l")
+  
   #Create Line Plot of Voltage over DateTime with y-axis label
   plot(DateTime,Voltage,xlab="datetime",ylab="Voltage",type="l")  
+  
   #Create Line Plot of Sub_metering_1 over DateTime with y-axis label
   plot(DateTime,Sub_metering_1,xlab="",ylab="Energy sub metering",type="l",col="black")
+  
   #Add a line to plot for Sub_metering_2 over DateTime
   lines(DateTime,Sub_metering_2,type="l",col="red")
+  
   #Add a line to plot for Sub_metering_3 over DateTime
   lines(DateTime,Sub_metering_3,type="l",col="blue")
+  
   #Add a legend to specify what each line on the graph associates with each Sub_metering
   legend("topright",lwd=1,col=c("black","red","blue"),legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
+  
   #Create Line Plot of Global Reactive Power over DateTime with y-axis label
   plot(DateTime,Global_reactive_power,xlab="datetime",ylab="Global_reactive_power",type="l")
 })#Close png device
